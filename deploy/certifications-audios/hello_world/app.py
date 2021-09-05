e098d17b08942557a03e86a3487004c2322954d3
Jorge Hoyos <jorgea.h93@gmail.com>
2021-09-05 15:08:32 -0500

    testing cd path

--- deploy/certifications-audios/hello_world/app.py
+++ deploy/certifications-audios/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world from aws",
+            "message": "hello world from aws.",
             # "location": ip.text.replace("\n", "")
         }),
     }
